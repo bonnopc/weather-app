66f0a2aa25107400163c69654b56fef5262e9b4b
Prosenjit Chowdhury <pc.chy007@gmail.com>
2018-12-24 01:52:51 +0600

    Added necessary packages and initialized App

---
+++ src/modules/home/components/HomeComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from "react";
+
+export default class HomeContainer extends Component {
+    render(){
+        return(
+            <Fragment>
+                Weather App Home
+            </Fragment>
+        )
+    }
+}
---
+++ src/route/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Switch, Route, withRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { store } from "../store";
+import HomeContainer from "../modules/home/containers/HomeContainer";
+
+class App extends Component {
+  render() {
+    return (
+    //   <Provider>
+          <Switch>
+            <Route path="/" component={HomeContainer} />
+          </Switch>
+    //   </Provider>
+    );
+  }
+}
+
+export default withRouter(App);
